89fb069b4d4f39ea38a9aed305bcbc41805e84be
Garrett Rowell <garrett@puppet.com>
2022-04-20 12:58:26 -0400

    change to file

--- site-modules/profile/manifests/example.pp
+++ site-modules/profile/manifests/example.pp
@@ -1,5 +1,8 @@
 class profile::example (
   $password_test# = lookup('test_password')
 ){
-  notify {"the test: ${password_test}":}
+  file { '/tmp/test.txt':
+    content => $password_test,
+  }
+  #notify {"the test: ${password_test}":}
 }
