41012add79332227861194a748d11105d78d8a6a
Garrett Rowell <garrett@puppet.com>
2022-04-20 13:49:50 -0400

    try sentitive new

--- site-modules/profile/manifests/example.pp
+++ site-modules/profile/manifests/example.pp
@@ -1,11 +1,15 @@
 class profile::example (
-  $password_test# = lookup('test_password')
+  $password_test,
+  $do_a_lookup = Sensitive.new(lookup('test_password'))
 ){
   file { '/tmp/test.txt':
     content => $password_test,
   }
   file { '/tmp/unwrap.txt':
     content => $password_test.unwrap,
+  }
+  file { '/tmp/sensitive.txt':
+    content => $do_a_lookup,
   }
 
   #notify {"the test: ${password_test}":}
