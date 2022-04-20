fc645fb432727e3eb0b64d5ef0794556b0830725
Garrett Rowell <garrett@puppet.com>
2022-04-20 14:21:27 -0400

    sanity check

--- site-modules/profile/manifests/example.pp
+++ site-modules/profile/manifests/example.pp
@@ -1,17 +1,17 @@
 class profile::example (
-  $password_test,
-  $do_a_lookup = Sensitive.new(lookup('test_password')),
+  #  $password_test,
+  #  $do_a_lookup = Sensitive.new(lookup('test_password')),
   $plain_lookup = lookup('test_password')
 ){
-  file { '/tmp/test.txt':
-    content => $password_test,
-  }
-  file { '/tmp/unwrap.txt':
-    content => $password_test.unwrap,
-  }
-  file { '/tmp/sensitive.txt':
-    content => $do_a_lookup,
-  }
+  #  file { '/tmp/test.txt':
+  #    content => $password_test,
+  #  }
+  #  file { '/tmp/unwrap.txt':
+  #    content => $password_test.unwrap,
+  #  }
+  #  file { '/tmp/sensitive.txt':
+  #    content => $do_a_lookup,
+  #  }
   file { '/tmp/plain.txt':
     content => $plain_lookup,
   }
