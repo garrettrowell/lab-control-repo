164620ece12a772716888f2527b29d89f730f51b
Garrett Rowell <garrett@puppet.com>
2022-04-22 16:03:09 -0400

    type

--- site-modules/deployments/lib/puppet/functions/deployments/servicenow_change_request.rb
+++ site-modules/deployments/lib/puppet/functions/deployments/servicenow_change_request.rb
@@ -22,7 +22,7 @@ Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
     required_param 'Boolean',   :auto_create_ci
     required_param 'String',    :ia_csv_export
     required_param 'Sring',     :gl_endpoint
-    required_param 'String',    :gl_oauth_token
+    required_param 'Sensitive', :gl_oauth_token
     required_param 'Hash',      :gl_now_usermap
   end
 
