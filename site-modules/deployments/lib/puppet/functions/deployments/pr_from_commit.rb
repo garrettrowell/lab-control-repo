ae7c713bcb6e481ad37809ac0c4335763e928b33
Garrett Rowell <garrett@puppet.com>
2022-04-22 11:33:10 -0400

    try to get number

--- site-modules/deployments/lib/puppet/functions/deployments/pr_from_commit.rb
+++ site-modules/deployments/lib/puppet/functions/deployments/pr_from_commit.rb
@@ -13,8 +13,11 @@ Puppet::Functions.create_function(:'deployments::pr_from_commit') do
   def pr_from_commit(repo)
     request_uri = "https://api.github.com/repos/#{repo['owner']}/#{repo['name']}/commits/#{repo['commit']}/pulls"
     request_response = make_request(request_uri, :get)
-    call_function('cd4pe_deployments::create_custom_deployment_event', "request_response: #{JSON.parse(request_response.body)}")
-
+    body = JSON.parse(request_response.body)
+    call_function('cd4pe_deployments::create_custom_deployment_event', "request_response: #{body}")
+    pr_number = body[0]['number']
+    call_function('cd4pe_deployments::create_custom_deployment_event', ": #{pr_number}")
+    return pr_number
   end
 
   def make_request(endpoint, type, payload = nil, content_type = 'application/json')
