8e0d71555c5645c79acd1af064be7fad9f24826e
Garrett Rowell <garrett@puppet.com>
2022-04-22 11:13:49 -0400

    fix a dumb

--- site-modules/deployments/lib/puppet/functions/deployments/pr_from_commit.rb
+++ site-modules/deployments/lib/puppet/functions/deployments/pr_from_commit.rb
@@ -13,9 +13,9 @@ Puppet::Functions.create_function(:'deployments::pr_from_commit') do
   def pr_from_commit(repo)
     request_uri = "https://api.github.com/repos/#{repo['owner']}/#{repo['name']}/commits/#{repo['commit']}/pulls"
     request_response = make_request(request_uri, :get)
-    call_function('cd4pe_deployments::create_custom_deployment_event', "request_response: #{JSON.parse(request_response)}")
+    call_function('cd4pe_deployments::create_custom_deployment_event', "request_response: #{JSON.parse(request_response.body)}")
   end
- 
+
   def make_request(endpoint, type, payload = nil, content_type = 'application/json')
     uri = URI.parse(endpoint)
     max_attempts = 3
