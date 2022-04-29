b76bc4472291ecaaa93bc08633cc40d319fedf53
Garrett Rowell <garrett@puppet.com>
2022-04-29 10:41:33 -0400

    add example pla

--- site-modules/adhoc/plans/myplan.pp
+++ site-modules/adhoc/plans/myplan.pp
@@ -1,9 +1,28 @@
-# This is a description for my plan
-plan adhoc::myplan(
-  # input parameters go here
-  TargetSpec $targets,
-) {
+# Plan that enforces base profile on all noop nodes
+plan adhoc::myplan {
 
-  # plan steps go here
+  # Query PuppetDB for nodes in noop
+  $pdb_result = puppetdb_query("inventory[certname] { facts.clientnoop = true }")
+  # extract the certnames into an array
+  $targets = $pdb_result.map |$r| { $r["certname"] }
 
+  # Gather facts about our nodes
+  $targets.apply_prep
+
+  # Ensure the Base profile gets applied
+  $apply_results = apply($targets) {
+    include profile::base
+    echo { "environment: ${environment}":}
+  }
+
+  out::message("teting: ${apply_results}")
+
+  # Print log messages from the report
+  $apply_results.each |$result| {
+    out::message("report: ${result.report}")
+
+    #$result.report['logs'].each |$log| {
+    #  out::message("${log['level'].upcase}: ${log['message']}")
+    #}
+  }
 }
