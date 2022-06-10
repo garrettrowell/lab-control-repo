769cf029a36c69fa6f5b8bf56d711ee28aafb1c4
Garrett Rowell <garrett@puppet.com>
2022-06-10 13:02:31 -0400

    go

--- site-modules/profile/manifests/windows.pp
+++ site-modules/profile/manifests/windows.pp
@@ -6,21 +6,25 @@ class profile::windows (
   $public_firewall_enabled  = 'false'
 
   dsc_firewallprofile { 'Disable_Windows_Firewall-Domain':
-    dsc_name    => 'Domain',
-    dsc_enabled => capitalize($domain_firewall_enabled),
+    dsc_name        => 'Domain',
+    dsc_enabled     => capitalize($domain_firewall_enabled),
+    validation_mode => resource,
   }
   dsc_firewallprofile { 'Disable_Windows_Firewall-Private':
-    dsc_name    => 'Private',
-    dsc_enabled => capitalize($private_firewall_enabled),
+    dsc_name        => 'Private',
+    dsc_enabled     => capitalize($private_firewall_enabled),
+    validation_mode => resource,
   }
   dsc_firewallprofile { 'Disable_Windows_Firewall-Public':
-    dsc_name    => 'Public',
-    dsc_enabled => capitalize($public_firewall_enabled),
+    dsc_name        => 'Public',
+    dsc_enabled     => capitalize($public_firewall_enabled),
+    validation_mode => resource,
   }
 
   dsc_netadapterbinding { 'DisableIPv6':
     dsc_componentid    => 'ms_tcpip6',
     dsc_interfacealias => '*',
     dsc_state          => 'Disabled',
+    validation_mode    => resource,
   }
 }
