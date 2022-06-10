2376b9d09b639443c9a7a0fc72ab8ea7fbd8acde
Garrett Rowell <garrett@puppet.com>
2022-06-10 11:57:17 -0400

    test

--- site-modules/profile/manifests/windows.pp
+++ site-modules/profile/manifests/windows.pp
@@ -1,4 +1,16 @@
 class profile::windows (
 ){
-  echo { 'imatest': }
+  $domain_firewall_enabled  = 'false'
+  $private_firewall_enabled = 'false'
+  $public_firewall_enabled  = 'false'
+
+  $d_f_e = $domain_firewall_enabled ? {
+    'true'  => 'Present',
+    'false' => 'Absent',
+  }
+
+  dsc_xdscfirewall { 'Domain':
+    dsc_ensure => $d_f_e,
+    dsc_zone   => 'Domain',
+  }
 }
