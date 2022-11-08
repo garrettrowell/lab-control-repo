1b76fb7d707ad66e2943ee7eca8045cf0705be40
Garrett Rowell <garrett@puppet.com>
2022-11-08 11:35:25 -0500

    try

--- site-modules/adhoc/tasks/migration.ps1
+++ site-modules/adhoc/tasks/migration.ps1
@@ -1,1 +1,16 @@
-# task content goes here
+# Useful locations
+$ssldir = puppet config print ssldir
+$statedir = puppet config print statedir
+
+# stop the service
+puppet resource service puppet ensure=stopped
+
+# backup ssldir and statedir
+Move-Item -Path $ssldir -Destination ${ssldir}-bak
+Move-Item -Path $statedir -Destination ${statedir}-bak
+
+# point to the new server
+puppet config set --section main server $PT_server
+
+# start the service again
+puppet resource service puppet ensure=running
