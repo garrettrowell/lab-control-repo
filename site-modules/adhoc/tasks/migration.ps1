c92deb1c1fe654b499d60e0361b3eb9e35d87049
Garrett Rowell <garrett@puppet.com>
2022-11-08 16:28:52 -0500

    ugh

--- site-modules/adhoc/tasks/migration.ps1
+++ site-modules/adhoc/tasks/migration.ps1
@@ -1,17 +1,17 @@
 param ($server)
 
 # Useful locations
-$version = (puppet --version)
-$ssldir = (puppet config print ssldir)
-$statedir = (puppet config print statedir)
-
-$testing = get-command puppet
-Write-Output (puppet --version)
-Write-Output "testing: $testing"
-Write-Output "server: $server"
-Write-Output "ssldir: $ssldir"
-Write-Output "statedir: $statedir"
-Write-Output "version: $version"
+#$version = (puppet --version)
+#$ssldir = (puppet config print ssldir)
+#$statedir = (puppet config print statedir)
+#
+#$testing = get-command puppet
+#Write-Output (puppet --version)
+#Write-Output "testing: $testing"
+#Write-Output "server: $server"
+#Write-Output "ssldir: $ssldir"
+#Write-Output "statedir: $statedir"
+#Write-Output "version: $version"
 
 # stop the service
 #puppet resource service puppet ensure=stopped
@@ -23,7 +23,7 @@ Write-Output "version: $version"
 #Move-Item -Path $statedir -Destination ${statedir}-bak
 #
 ## point to the new server
-#puppet config set --section main server $PT_server
+puppet config set --section main server $PT_server
 #
 ## start the service again
 #puppet resource service puppet ensure=running
