6c4b6536597e5c3ca132ee084b203ee95c0fa4ba
Garrett Rowell <garrett@puppet.com>
2022-11-08 11:05:19 -0500

    again

--- site-modules/adhoc/tasks/migration.sh
+++ site-modules/adhoc/tasks/migration.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ssldir=$(puppet config print ssldir)
+statedir=$(puppet config print statedir)
 puppet resource service puppet ensure=stopped
-echo $ssldir
-#mv $(puppet config print ssldir) $(puppet config print ssldir)-bak
-#mv $(puppet config print statedir) $(puppet config print statedir)-bak
+mv $ssldir ${ssldir}-bak
+mv $statedir ${statedir}-bak
+echo $PT_server
 puppet config set --section main server $PT_server
 puppet resource service puppet ensure=running
