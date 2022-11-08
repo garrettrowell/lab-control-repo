41499cee7c5123d676a2d001bbfa8782420f77e4
Garrett Rowell <garrett@puppet.com>
2022-11-08 11:22:18 -0500

    nix good

--- site-modules/adhoc/tasks/migration.sh
+++ site-modules/adhoc/tasks/migration.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Useful locations
 ssldir=$(puppet config print ssldir)
 statedir=$(puppet config print statedir)
+
+# stop the service
 puppet resource service puppet ensure=stopped
+
+# backup ssldir and statedir
 mv $ssldir ${ssldir}-bak
 mv $statedir ${statedir}-bak
-echo $PT_server
+
+# point to the new server
 puppet config set --section main server $PT_server
+
+# start the service again
 puppet resource service puppet ensure=running
