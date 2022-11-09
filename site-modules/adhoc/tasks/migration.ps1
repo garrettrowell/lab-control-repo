4dbe27455bb8b73ecd66f2de8dc9eaf4cfcde4a3
Garrett Rowell <garrett@puppet.com>
2022-11-09 09:33:57 -0500

    baklj

--- site-modules/adhoc/tasks/migration.ps1
+++ site-modules/adhoc/tasks/migration.ps1
@@ -6,20 +6,14 @@ $puppet_bin_dir = Join-Path ([Environment]::GetFolderPath('ProgramFiles')) 'Pupp
 # Useful locations
 $ssldir = & $puppet_bin_dir\puppet config print ssldir
 $statedir = & $puppet_bin_dir\puppet config print statedir
-#
-#$testing = get-command puppet
-#Write-Output (puppet --version)
-#Write-Output "testing: $testing"
-#Write-Output "server: $server"
-Write-Output "ssldir: $ssldir"
-Write-Output "statedir: $statedir"
-#Write-Output "version: $version"
 
 # stop the service
 & $puppet_bin_dir\puppet resource service puppet ensure=stopped
 
 ## backup ssldir and statedir
+Write-Output "moving $ssldir to $ssldir`-bak"
 Move-Item -Path $ssldir -Destination "$ssldir`-bak"
+Write-Output "moving $statedir to $statedir`-bak"
 Move-Item -Path $statedir -Destination "$statedir`-bak"
 
 ## point to the new server
