9b6786fa61d749c2cbe3558ee10b54faea050961
Garrett Rowell <garrett@puppet.com>
2022-11-11 12:00:23 -0500

    whee

--- site-modules/profile/lib/facter/csr_attrs.rb
+++ site-modules/profile/lib/facter/csr_attrs.rb
@@ -4,9 +4,10 @@ Facter.add(:csr_attrs) do
     csr_attr_file = "#{confdir}/csr_attributes.yaml"
     parsed_file = YAML.load(File.read(csr_attr_file))
     if parsed_file.key?('extension_requests')
-      {
-        'pp_role' => parsed_file['extension_requests'].key?('pp_role') ? parsed_file['extension_requests']['pp_role'] : nil
-      }
+      parsed_file['extension_requests']
+#      {
+#        'pp_role' => parsed_file['extension_requests'].key?('pp_role') ? parsed_file['extension_requests']['pp_role'] : nil
+#      }
     end
   end
 end
