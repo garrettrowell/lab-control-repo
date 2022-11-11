c37a4708a2e4cd5a565a0235463538ae2f7fa01c
Garrett Rowell <garrett@puppet.com>
2022-11-11 11:48:16 -0500

    try parsing

--- site-modules/profile/lib/facter/csr_attrs.rb
+++ site-modules/profile/lib/facter/csr_attrs.rb
@@ -2,6 +2,6 @@ Facter.add(:csr_attrs) do
   setcode do
     confdir = Facter.value(:puppet_confdir)
     csr_attr_file = "#{confdir}/csr_attributes.yaml"
-    csr_attr_file
+    YAML.load(File.read(csr_attr_file))
   end
 end
