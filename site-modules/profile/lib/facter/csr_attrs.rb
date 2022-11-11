2f3117e3a8d611c2416b7154fc85f4367e2db8f5
Garrett Rowell <garrett@puppet.com>
2022-11-11 11:56:36 -0500

    testing

--- site-modules/profile/lib/facter/csr_attrs.rb
+++ site-modules/profile/lib/facter/csr_attrs.rb
@@ -2,6 +2,11 @@ Facter.add(:csr_attrs) do
   setcode do
     confdir = Facter.value(:puppet_confdir)
     csr_attr_file = "#{confdir}/csr_attributes.yaml"
-    YAML.load(File.read(csr_attr_file))
+    parsed_file = YAML.load(File.read(csr_attr_file))
+    if parsed_file.key?('extension_requests')
+      {
+        'pp_role' => parsed_file['extension_requests'].key?('pp_role') ? parsed_file['extension_requests']['pp_role'] : nil
+      }
+    end
   end
 end
