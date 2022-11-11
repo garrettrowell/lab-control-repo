c0149ea7fdc516249bcedfa065b0159afce85866
Garrett Rowell <garrett@puppet.com>
2022-11-11 14:33:26 -0500

    try this

--- site-modules/profile/lib/facter/csr_attrs.rb
+++ site-modules/profile/lib/facter/csr_attrs.rb
@@ -2,12 +2,15 @@ Facter.add(:csr_attrs) do
   setcode do
     confdir = Facter.value(:puppet_confdir)
     csr_attr_file = "#{confdir}/csr_attributes.yaml"
-    parsed_file = YAML.load(File.read(csr_attr_file))
-    if parsed_file.key?('extension_requests')
-      parsed_file['extension_requests']
-#      {
-#        'pp_role' => parsed_file['extension_requests'].key?('pp_role') ? parsed_file['extension_requests']['pp_role'] : nil
-#      }
-    end
-  end
-end
+
+    if File.file?(csr_attr_file)
+      parsed_file = YAML.load(File.read(csr_attr_file))
+      if parsed_file.key?('extension_requests')
+        parsed_file['extension_requests']
+        if parsed_file['extension_requests'].key?('pp_role')
+          parsed_file['extension_requests']['pp_role'].split('::')
+        end # if pp_role
+      end # if extension requests
+    end # if file exists
+  end # setcode do
+end # facter.add
