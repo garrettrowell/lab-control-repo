d68f24237f658c9677a9ea7c267fc7b97b205e3e
root <root@ip-172-31-129-80.us-east-2.compute.internal>
2023-02-17 16:57:07 +0000

    asfd

--- site-modules/adhoc/plans/env_test.pp
+++ site-modules/adhoc/plans/env_test.pp
@@ -5,8 +5,13 @@ plan adhoc::env_test(
 
   run_command('whoami', $targets)
   run_command('set', $targets)
+
   $path = system::env('PATH')
   out::message("the path: ${path}")
+  $hostname = system::env('HOSTNAME')
+  out::message("the hostname: ${hostname}")
+  $hosttype = system::env('HOSTTYPE')
+  out::message("the hosttype: ${hosttype}")
 
 #  run_command('export SOME_ENV_VAR=imatest', $targets)
 #  run_command('env', $targets)
