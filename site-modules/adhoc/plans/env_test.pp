c28356aed0d59e4b18e93e51001524e35a105794
root <root@ip-172-31-129-80.us-east-2.compute.internal>
2023-02-17 17:24:51 +0000

    sdafsdf

--- site-modules/adhoc/plans/env_test.pp
+++ site-modules/adhoc/plans/env_test.pp
@@ -10,7 +10,24 @@ plan adhoc::env_test(
   out::message("the path: ${path}")
 
   get_targets($targets).each |$target| {
-    out::message("the keys: ${target.keys}")
+    out::message("the config: ${target.config}")
+    out::message("the facts: ${target.facts}")
+    out::message("the features: ${target.features}")
+    out::message("the host: ${target.host}")
+    out::message("the name: ${target.name}")
+    out::message("the password: ${target.password}")
+    out::message("the plugin_hooks: ${target.plugin_hooks}")
+    out::message("the port: ${target.port}")
+    out::message("the protocol: ${target.protocol}")
+    out::message("the resources: ${target.resources}")
+    out::message("the safe_name: ${target.safe_name}")
+    out::message("the target_alias: ${target.target_alias}")
+    out::message("the transport: ${target.transport}")
+    out::message("the transport_config: ${target.transport_config}")
+    out::message("the uri: ${target.uri}")
+    out::message("the user: ${target.user}")
+    out::message("the vars: ${target.vars}")
+
   }
 
 #  run_command('export SOME_ENV_VAR=imatest', $targets)
